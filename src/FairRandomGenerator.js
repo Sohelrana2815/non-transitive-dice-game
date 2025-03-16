07e7462bd98f83526ba155f21cba8553c2448a78
Sohelrana2815 <sohel152302@gmail.com>
2025-03-16 11:36:28 +0600

    generate crypto secure nums

--- src/FairRandomGenerator.js
+++ src/FairRandomGenerator.js
@@ -1,1 +1,14 @@
-const crypto = require ('crypto');
+const crypto = require("crypto");
+
+class FairRandomGenerator {
+  generate(max) {
+    const key = crypto.randomBytes(32);
+    // Range of random number (0..1)
+    const number = crypto.randomInt(0, max, 1);
+    const hmac = crypto
+      .createHmac("sha3-256", key)
+      .update(number.toString())
+      .digest("hex");
+    
+  }
+}
