204fdd58cf7f8ec46aa002cdb086a41e06f85232
Sohelrana2815 <sohel152302@gmail.com>
2025-03-16 10:02:50 +0600

    return dice face value at given index like (0-5)

---
+++ src/Dice.js
@@ -0,0 +1,15 @@
+class Dice {
+  constructor(faces) {
+    this.faces = faces;
+  }
+
+  roll(index) {
+    return this.faces[index];
+  }
+
+  toString() {
+    return `[${this.faces.join(",")}]`;
+  }
+}
+
+module.exports = Dice;
